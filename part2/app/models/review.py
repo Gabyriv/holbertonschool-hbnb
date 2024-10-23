d2f13aee84d76958cc03cbc0382961acd3ec3059
Gabriel <gabyrivm06@gmail.com>
2024-10-23 14:51:07 -0400

    Adding base classes for the Users, Places, Reviews and Amenities. Task 1.

---
+++ part2/app/models/review.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from app.models.base_model import BaseModel
+from app.models.user import User
+from app.models.place import Place
+
+
+class Review(BaseModel):
+    def __init__(self, text, rating, place, user):
+        super().__init__()
+
+        if not 1 <= rating <= 5:
+            raise ValueError("Rating must be between 1 and 5.")
+
+        if not isinstance(place, Place):
+            raise TypeError("Place must be validated.")
+
+        if not isinstance(user, User):
+            raise TypeError("User must be validated.")
+
+
+        self.text = text
+        self.rating = rating
+        self.place = Place(place)
+        self.user = User(user)
