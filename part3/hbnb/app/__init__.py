7395b8f8c95cfbad4d9fc735576c423abe2c9be1
Gabriel <gabyrivm06@gmail.com>
2024-11-01 10:52:34 -0400

    adding the authentication namespace for in the app init

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,6 +6,7 @@ from hbnb.app.api.v1.users import api as users_ns
 from hbnb.app.api.v1.amenities import api as amenities_ns
 from hbnb.app.api.v1.places import api as places_ns
 from hbnb.app.api.v1.reviews import api as reviews_ns
+from hbnb.app.api.v1.auth import api as auth_ns
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
@@ -22,6 +23,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     bcrypt.init_app(app)
     jwt.init_app(app)
