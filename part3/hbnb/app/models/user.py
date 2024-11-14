72057e8f39318f18645b5620127b489007ce88a4
Gabriel <gabyrivm06@gmail.com>
2024-11-14 11:15:45 -0400

    Modifying the User Model to Include Password Hashing

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -3,18 +3,21 @@
 from datetime import datetime
 from hbnb.app.models.base_model import BaseModel
 import re
+from hbnb.app import bcrypt
 
 # This variable is used to validate the email format
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
+
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = self._validate_first_name(first_name)
         self.last_name = self._validate_last_name(last_name)
         self.email = self._validate_email(email)
         self.is_admin = is_admin
         self.places = []
+        self.password = password
 
     def _validate_email(self, email):
         if not re.fullmatch(regex, email):
@@ -33,3 +36,11 @@ class User(BaseModel):
         if len(last_name) == 0:
             raise ValueError("Last name cannot be empty")
         return last_name
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
