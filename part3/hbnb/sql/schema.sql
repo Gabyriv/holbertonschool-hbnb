136588011faaaf76effc1a5219bdf281608a2b26
Gabriel <gabyrivm06@gmail.com>
2024-11-17 18:19:04 -0400

    SQL changed the id's in the amenities testing data

--- part3/hbnb/sql/schema.sql
+++ part3/hbnb/sql/schema.sql
@@ -75,6 +75,6 @@ INSERT INTO users (
 
 -- Insert initial amenities
 INSERT INTO amenities (id, name) VALUES
-    (uuid(), 'WiFi'),
-    (uuid(), 'Swimming Pool'),
-    (uuid(), 'Air Conditioning');
+    ('550e8400-e29b-41d4-a716-446655440000', 'WiFi'),
+    ('550e8400-e29b-41d4-a716-446655440001', 'Swimming Pool'),
+    ('550e8400-e29b-41d4-a716-446655440002', 'Air Conditioning');
